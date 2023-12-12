a507b3cf300ffecdf06297aec412393461e4b46e
YUU522 <kuromu2750@gmail.com>
2023-12-12 14:15:39 +0900

    偶数昇順

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -4,7 +4,12 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		 System.out.println(5+5);
+		 for(int i=1; i<=10; i++) {
+			 if(i%2==0) {
+				 System.out.println(i);
+			 }
+		 }
+		 
 	}
 
 }
