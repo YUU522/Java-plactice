01d5d18447f2c751fcf5a48ec4ef5e43fe0d0618
YUU522 <kuromu2750@gmail.com>
2023-12-12 14:24:55 +0900

    偶数の個数と合計

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -4,12 +4,16 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		int m=0;
+		int sum=0;
 		 for(int i=1; i<=10; i++) {
 			 if(i%2==0) {
-				 System.out.println(i);
+				 sum+=i;
+				 m++;
 			 }
 		 }
-		 
+		 System.out.println("偶数の個数"+m);
+		 System.out.println("偶数の合計"+sum);
 	}
 
 }
